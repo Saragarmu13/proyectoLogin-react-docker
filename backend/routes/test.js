8146aa92c010df6385fba1e4755599308316845c
Saragarmu13 <saragm1994@gmail.com>
2025-07-08 12:35:48 +0200

    Último commit con cambios finales

---
+++ backend/routes/test.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const router = express.Router();
+const pool = require('../bd');
+
+router.get('/test-db', async (req, res) => {
+  try {
+    const [rows] = await pool.query('SELECT 1');
+    res.send('Conexión a la base de datos OK');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Error en la conexión a la BD');
+  }
+});
+
+module.exports = router;
